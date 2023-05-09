69ad833c9a919df985a818ed684e6d45b2c4f490
Addison <addison@gitbook.com>
2023-05-09 14:53:32 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { getCommandParams, postError } from './utils'
 import { publishTranslate } from './translate'
 import { authorizeUser } from './git'
+import { getInput } from '@actions/core'
 
 async function main() {
   const isAuthorized = await authorizeUser()
   if (!isAuthorized) {
     await postError('You have no permission to use this bot.')
   }
+
+  // Array of languages set in action workflow
+  const languages = getInput('languages')
+  console.log(languages)
 
   const { inputFilePath, outputFilePath, targetLang } = await getCommandParams()
   await publishTranslate(inputFilePath, outputFilePath, targetLang)
